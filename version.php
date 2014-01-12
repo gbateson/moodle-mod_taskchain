26bfca3a286e6c993e3f22912aecaabc094a151d
Gordon Bateson <gorodnbateson@gmail.com>
2014-01-12 20:18:08 +0900

    mod/taskchain fix reading of sibling files required for Xerte files

--- version.php
+++ version.php
@@ -31,6 +31,6 @@ defined('MOODLE_INTERNAL') || die();
 $module->cron      = 0; // 60
 $module->component = 'mod_taskchain';
 $module->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
-$module->release   = '2014.01.12 (68)';
-$module->version   = 2014011268;
+$module->release   = '2014.01.12 (69)';
+$module->version   = 2014011269;
 $module->requires  = 2010112400; // Moodle 2.0
