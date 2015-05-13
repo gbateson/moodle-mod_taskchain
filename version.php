6f16bbd9611385861227dce13e3ff2bfcdcc82d7
Gordon Bateson <gorodnbateson@gmail.com>
2015-05-13 23:59:07 +0900

    mod/taskchain allow detection of Moodle version from $CFG->yui3version, if $CFG->release is not available, for example when installing from the CLI using PHPunit

--- version.php
+++ version.php
@@ -28,7 +28,19 @@
 // prevent direct access to this script
 defined('MOODLE_INTERNAL') || die();
 
-if (floatval($GLOBALS['CFG']->release) <= 2.6) {
+if (empty($CFG)) {
+    global $CFG;
+}
+
+if (isset($CFG->release)) {
+    $moodle_26 = (floatval($CFG->release) <= 2.6);
+} else if (isset($CFG->yui3version)) {
+    $moodle_26 = (floatval($CFG->yui3version) <= 3.13);
+} else {
+    $moodle_26 = false;
+}
+
+if ($moodle_26) {
     $plugin = new stdClass();
 }
 
@@ -36,9 +48,9 @@ $plugin->cron      = 0; // 60
 $plugin->component = 'mod_taskchain';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->release   = '2015.04.12 (74)';
-$plugin->version   = 2015041274;
+$plugin->release   = '2015.05.13 (75)';
+$plugin->version   = 2015051375;
 
-if (floatval($GLOBALS['CFG']->release) <= 2.6) {
+if ($moodle_26) {
     $module = clone($plugin);
 }
