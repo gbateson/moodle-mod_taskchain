1baf9b4c26fbb078c119b6091f2d85e8a1e69347
Gordon Bateson <gordonbateson@gmail.com>
2015-10-01 18:06:36 +0900

    mod/taskchain disable FEATURE_CONTROLS_GRADE_VISIBILITY as it was not being used

--- lib.php
+++ lib.php
@@ -59,7 +59,7 @@ function taskchain_supports($feature) {
         'FEATURE_COMMENT'          => true,
         'FEATURE_COMPLETION_HAS_RULES' => true,
         'FEATURE_COMPLETION_TRACKS_VIEWS' => true,
-        'FEATURE_CONTROLS_GRADE_VISIBILITY' => true,
+        'FEATURE_CONTROLS_GRADE_VISIBILITY' => false,
         'FEATURE_GRADE_HAS_GRADE'  => true, // default=false
         'FEATURE_GRADE_OUTCOMES'   => true,
         'FEATURE_GROUPINGS'        => true, // default=false
