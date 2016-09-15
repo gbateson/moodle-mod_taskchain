3b814bef76205504bfd8d40269bd2def3ad68c4b
Gordon Bateson <gordonbateson@gmail.com>
2016-09-15 10:31:42 +0900

    mod_taskchain handle self-modified HP html files that have no Begin|EndSubmissionsForm markers

--- version.php
+++ version.php
@@ -40,8 +40,8 @@ $plugin->cron      = 0; // 60
 $plugin->component = 'mod_taskchain';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->release   = '2016-07-24 (25)';
-$plugin->version   = 2016072425;
+$plugin->release   = '2016-09-15 (26)';
+$plugin->version   = 2016091526;
 
 if (empty($CFG->branch) || $CFG->branch <= 26) {
     $module = clone($plugin);
