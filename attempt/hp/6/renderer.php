524d66e8f0e14023142c79fa90d694229978b1cd
Gordon Bateson <gordonbateson@gmail.com>
2018-05-25 12:45:57 +0900

    mod/taskchain add to <open-text> tags to JCloze XML to account for new behavior of XML parser in Moodle >= 3.5

--- attempt/hp/6/renderer.php
+++ attempt/hp/6/renderer.php
@@ -3460,24 +3460,21 @@ class mod_taskchain_attempt_hp_6_renderer extends mod_taskchain_attempt_hp_rende
         }
 
         // initialize loop values
-        $q = 0;
+        $i = 0;
         $tags = 'data,gap-fill';
-        $question_record = "$tags,question-record";
-
-        // initialize loop values
-        $q = 0;
-        $tags = 'data,gap-fill';
+        $open_text = "$tags,open-text";
         $question_record = "$tags,question-record";
 
         // loop through text and gaps
         $looping = true;
         while ($looping) {
-            $text = $this->TC->task->source->xml_value($tags, "[0]['#'][$q]");
             $gap = '';
-            if (($question="[$q]['#']") && $this->TC->task->source->xml_value($question_record, $question)) {
-                $gap .= '<span class="GapSpan" id="GapSpan'.$q.'">';
+            $item = "[$i]['#']";
+            $text = $this->TC->task->source->xml_value($open_text, $item);
+            if ($this->TC->task->source->xml_value($question_record, $item)) {
+                $gap .= '<span class="GapSpan" id="GapSpan'.$i.'">';
                 if ($this->use_DropDownList()) {
-                    $gap .= '<select id="Gap'.$q.'"><option value=""></option>'.$dropdownlist.'</select>';
+                    $gap .= '<select id="Gap'.$i.'"><option value=""></option>'.$dropdownlist.'</select>';
                 } else {
                     // minimum gap size
                     if (! $gapsize = $this->TC->task->source->xml_value_int($this->TC->task->source->hbs_software.'-config-file,'.$this->TC->task->source->hbs_tasktype.',minimum-gap-size')) {
@@ -3486,19 +3483,19 @@ class mod_taskchain_attempt_hp_6_renderer extends mod_taskchain_attempt_hp_rende
 
                     // increase gap size to length of longest answer for this gap
                     $a = 0;
-                    while (($answer=$question."['answer'][$a]['#']") && $this->TC->task->source->xml_value($question_record, $answer)) {
+                    while (($answer=$item."['answer'][$a]['#']") && $this->TC->task->source->xml_value($question_record, $answer)) {
                         $answertext = $this->TC->task->source->xml_value($question_record,  $answer."['text'][0]['#']");
                         $answertext = preg_replace('/&[#a-zA-Z0-9]+;/', 'x', $answertext);
                         $gapsize = max($gapsize, strlen($answertext));
                         $a++;
                     }
 
-                    $gap .= '<input type="text" id="Gap'.$q.'" onfocus="TrackFocus('.$q.')" onblur="LeaveGap()" class="GapBox" size="'.$gapsize.'"></input>';
+                    $gap .= '<input type="text" id="Gap'.$i.'" onfocus="TrackFocus('.$i.')" onblur="LeaveGap()" class="GapBox" size="'.$gapsize.'"></input>';
                 }
                 if ($includeclues) {
-                    $clue = $this->TC->task->source->xml_value($question_record, $question."['clue'][0]['#']");
+                    $clue = $this->TC->task->source->xml_value($question_record, $item."['clue'][0]['#']");
                     if (strlen($clue)) {
-                        $gap .= '<button style="line-height: 1.0" class="FuncButton" onfocus="FuncBtnOver(this)" onmouseover="FuncBtnOver(this)" onblur="FuncBtnOut(this)" onmouseout="FuncBtnOut(this)" onmousedown="FuncBtnDown(this)" onmouseup="FuncBtnOut(this)" onclick="ShowClue('.$q.')">'.$cluecaption.'</button>';
+                        $gap .= '<button style="line-height: 1.0" class="FuncButton" onfocus="FuncBtnOver(this)" onmouseover="FuncBtnOver(this)" onblur="FuncBtnOut(this)" onmouseout="FuncBtnOut(this)" onmousedown="FuncBtnDown(this)" onmouseup="FuncBtnOut(this)" onclick="ShowClue('.$i.')">'.$cluecaption.'</button>';
                     }
                 }
                 $gap .= '</span>';
@@ -3509,13 +3506,13 @@ class mod_taskchain_attempt_hp_6_renderer extends mod_taskchain_attempt_hp_rende
                 } else {
                     $str .= $text.$gap;
                 }
-                $q++;
+                $i++;
             } else {
                 // no text or gap, so force end of loop
                 $looping = false;
             }
         }
-        if ($q==0) {
+        if ($i==0) {
             // oops, no gaps found!
             return $this->TC->task->source->xml_value($tags);
         } else {
