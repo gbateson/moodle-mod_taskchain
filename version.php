012de4d64fc9347dc1a5558c89adcee3f372ea6a
Gordon Bateson <gordonbateson@gmail.com>
2019-11-10 21:06:54 +0900

    mod_taskchain standardize layout and spelling in CHANGES.txt

--- version.php
+++ version.php
@@ -40,8 +40,8 @@ $plugin->cron      = 0; // 60
 $plugin->component = 'mod_taskchain';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2019103186;
-$plugin->release   = '2019-10-31 (86)';
+$plugin->version   = 2019111087;
+$plugin->release   = '2019-11-10 (87)';
 
 if (isset($CFG->yui3version) && version_compare($CFG->yui3version, '3.15.0') < 0) {
     $module = clone($plugin); // Moodle <= 2.6
