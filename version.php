f96adfe442817bcdf225cc4675b568e6d3d8e0ac
Gordon Bateson <gordonbateson@gmail.com>
2020-03-01 11:12:38 +0900

    mod_taskchain include current version in tool to redo upgrade

--- version.php
+++ version.php
@@ -40,8 +40,8 @@ $plugin->cron      = 0; // 60
 $plugin->component = 'mod_taskchain';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2019111087;
-$plugin->release   = '2019-11-10 (87)';
+$plugin->version   = 2020030188;
+$plugin->release   = '2020-03-01 (88)';
 
 if (isset($CFG->yui3version) && version_compare($CFG->yui3version, '3.15.0') < 0) {
     $module = clone($plugin); // Moodle <= 2.6
