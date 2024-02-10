2aa106f2053e1bca2516aa5cf9339b35c12b4548
Gordon Bateson <gordonbateson@gmail.com>
2024-02-10 13:19:10 +0900

    mod_taskchain more efficient code to fetch 'modgrade' string in Moodle >= 2.8 and 'grade' string in Moodle <= 2.7.

--- report/userfiltering.php
+++ report/userfiltering.php
@@ -62,7 +62,10 @@ class taskchain_user_filtering extends user_filtering {
             case 'grouping':
                 return new taskchain_filter_group($fieldname, $advanced, $default);
             case 'grade':
-                $label = get_string('grade', 'grades');
+                // In Moodle >= 2.7, we use the "modgrade" string.
+                // In Moodle <= 2.6, we use simply "grade".
+                $modgrade = get_string_manager()->string_exists('modgrade', 'grades');
+                $label = get_string($modgrade ? 'modgrade' : 'grade', 'grades');
                 return new taskchain_filter_grade($fieldname, $label, $advanced, $default);
             case 'timemodified':
                 $label = get_string('time', 'quiz');
